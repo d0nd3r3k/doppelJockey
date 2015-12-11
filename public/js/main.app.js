0bbd52a9d967f4b2fa4ddc9d1d821ce5eff6867a
Donald Derek <donaldderek.haddad@gmail.com>
2015-12-10 22:29:58 -0500

    added socket remote

--- public/js/main.app.js
+++ public/js/main.app.js
@@ -152,6 +152,19 @@ socket.on('connect', function(){
         socket.emit('control', guidat);
 });
 
+socket.on('v', function(data){
+        console.log("move vertical: %d", data)
+});
+
+socket.on('h', function(data){
+        console.log("move horizontal: %d", data)
+});
+
+socket.on('t', function(data){
+        console.log(data);
+});
+
+
 socket.on('apply', function (data) {
     //Add testing controls handled by dat gui
     guidat.scale = data;
@@ -410,6 +423,119 @@ function render(dt) {
  * Helper Functions
  */
 
+ function pauseTrack(id){
+     tracks[id].source.disconnect();
+ }
+
+ function unpauseTrack(id){
+     tracks[id].source.connect(tracks[id].volume)
+ }
+
+ function drawCenterCross(){
+     debugContext.moveTo( -10, 0 );
+ 	debugContext.lineTo( 10, 0 );
+ 	debugContext.moveTo( 0, -10 );
+ 	debugContext.lineTo( 0, 10 );
+ }
+
+ function drawDebugSphere(sphere, color){
+     debugContext.beginPath();
+     debugContext.rect(-sphere.position.z+122,(sphere.position.x-122)/2.5,10,10);
+     debugContext.strokeStyle = color;
+     debugContext.closePath();
+ 	debugContext.stroke();
+ }
+
+ function drawDebugPlay(sphere, opp, isPaused){
+     if (typeof(isPaused)==='undefined') isPaused = false;
+     debugContext.beginPath();
+     if(opp)
+         debugContext.rect(-sphere.position.z+125,(sphere.position.x-98)/2.5,4,4);
+     else
+         debugContext.rect(-sphere.position.z+125,(sphere.position.x-132)/2.5,4,4);
+
+     if(isPaused)
+         debugContext.strokeStyle = 'blue';
+     else
+         debugContext.strokeStyle = 'green';
+
+     debugContext.closePath();
+ 	debugContext.stroke();
+ }
+
+ function whichQuad(){
+     if((player.position.x <= 127) && (player.position.z >= 0) && (player.position.z <= 127))
+         return 0;
+     if((player.position.x <= 127) && (player.position.z > 127) && (player.position.z <= 254))
+         return 1;
+     if((player.position.x > 127) && (player.position.z >= 0) && (player.position.z <= 127))
+         return 2;
+     if((player.position.x > 127) && (player.position.z >= 127) && (player.position.z <= 254))
+         return 3;
+ }
+
+ function visualizeSingleCube(i, v){
+
+     var scale = v+getRandomInt(1,5)/10*guidat.scale;
+     var index = i%blocks.length;
+     blocks[index].scale.set(scale, scale, scale);
+
+     if(v < 0.8)
+         blocks[index].material = materials.basicRed;
+     else if ( v > 0.8 && v < 1.05)
+         blocks[index].material = materials.basicBlack;
+     else
+         blocks[index].material = materials.basicWhite;
+ }
+
+ function spawnCube(scale, x, y, z){
+     geometries.scale = scale;
+     var cube = new THREE.Mesh( geometries.basicCube, materials.basicBlack );
+     cube.position.set(x, y, z);
+     blocks.push(cube);
+     scene.add(cube);
+ }
+
+ function spawnCubismTest(){
+     spawnCube(1, 10, 10, 10);
+     spawnCube(2, 20, 10, 10);
+     spawnCube(4, 10, 20, 10);
+     spawnCube(3, 10, 10, 20);
+ }
+
+ function spawnSphere(scale, x, y, z, isTrack){
+     var basicSphere = new THREE.SphereGeometry(scale, 32, 32);
+     var material;
+
+     if (isTrack)
+         material = new THREE.MeshBasicMaterial( { color: 0xfefefe, wireframe:true,transparent: true, opacity: 0.2 } );
+     else
+         material = new THREE.MeshBasicMaterial( { color: 0xfefefe, wireframe:true,transparent: true, opacity: 0.02 } );
+
+     var s = new THREE.Mesh( basicSphere, material );
+     s.position.set(x, y, z);
+     scene.add(s);
+
+     if(isTrack)
+         spheres.push(s);
+ }
+
+ function animateBlock(time){
+     blocks[0].position.set(10, Math.sin(time*0.013)*10+5, 10);
+     blocks[1].position.set(20, Math.sin(time*0.01+10)*10+5, 10);
+     blocks[2].position.set(40, Math.sin(time*0.003)*10+1, 10);
+     blocks[3].position.set(40, Math.sin(time*0.01)*10+3, 20);
+ }
+
+ function scaleBlocks(scale){
+     for(var i=0; i<blocks.length; i++){
+             blocks[i].scale.set(Math.random()*2*scale, Math.random()*4*scale, Math.random()*2*scale);
+     }
+ }
+
+ function turnBlock(){
+     blocks[3].rotation.y += 0.1;
+ }
 function playTrack(id){
     console.log("Trying to play new song: %s", tracks[id].filename);
     tracks[id].isPlaying=true;
@@ -423,6 +549,7 @@ function playTrack(id){
     tracks[id].panner.setPosition(tracks[id].x,tracks[id].y,tracks[id].z);
 
     tracks[id].source.start(0);
+    sendTrackStatus(id);
 }
 
 function applyFilter(id, type){
@@ -443,10 +570,12 @@ function applyFilter(id, type){
 
     tracks[id].source.connect(tracks[id].filter);
     tracks[id].filter.connect(tracks[id].volume);
+    sendTrackStatus(id);
 }
 
 function changeCutOff(id, filter_id){
     tracks[id].filter.frequency.value = tracks[id].fx[filter_id].cutoff;
+    sendTrackStatus(id);
 }
 
 function turnOffFilter(id, type){
@@ -461,27 +590,54 @@ function turnOffFilter(id, type){
     if(tracks[id].filter)
         tracks[id].filter.disconnect()
     tracks[id].source.connect(tracks[id].volume);
+    sendTrackStatus(id);
 }
 
 function stopTrack(id){
     tracks[id].isPlaying=false;
     tracks[id].source.stop();
+    sendTrackStatus(id);
 }
 
 function pauseTrack(id){
     tracks[id].isPlaying=100;
     tracks[id].source.disconnect();
+    sendTrackStatus(id);
 }
 
 function unpauseTrack(id){
     tracks[id].isPlaying=true;
-    tracks[id].source.connect(tracks[id].volume)
+    tracks[id].source.connect(tracks[id].volume);
+    sendTrackStatus(id);
 }
 
 
 /*
  * Util functions
  */
+
+function sendPlayerPosition(){
+    socket.emit('position', player.position);
+}
+
+function sendVerticalSlider(p){
+    socket.emit('vertical',p);
+}
+
+function sendHorizontalSlider(p){
+    socket.emit('horizontal',p);
+}
+
+function sendTrackStatus(id){
+    var d = {};
+    var t = tracks[id];
+    d.id = id;
+    d.filename = t.filename.split("/")[1];
+    d.fx = t.fx;
+    d.position = [t.x, t.y, t.z];
+    d.status = t.isPlaying;
+    socket.emit('track',d);
+}
 
  function sendMessage(data){
      console.log(data)
@@ -626,12 +782,14 @@ function onMIDIMessage( event ) {
     console.log("a: %s, f: %s",action, force);
     switch (action) {
         case 10:
+            sendHorizontalSlider(force);
             player.position.set(player.position.x, player.position.y, 2*force);
             break;
         case 22:
             guidat.scale = force;
             break;
         case 23:
+            sendVerticalSlider(force);
             player.position.set(256-2*force, player.position.y, player.position.z);
             break;
         case 24:
